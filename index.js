f9e7ae4a759e7356b6e550d233c8429e836f7a31
Ian Mike <ian.kaibi@student.moringaschool.com>
2022-08-18 17:19:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+// const log = console.log;
+
+function saturdayFun(defaultActivity = 'roller-skate'){
+    return `This Saturday, I want to ${defaultActivity}!`
+}
+// log(saturdayFun('bathe my dog'))
+
+
+function mondayWork(defaultActivity = 'go to the office'){
+    return `This Monday, I will ${defaultActivity}.`
+}
+
+// log(mondayWork('work from home'))
+
+function wrapAdjective(wrapper = '*'){
+    return function (emphatic = 'special'){
+        return`You are ${wrapper}${emphatic}${wrapper}!`
+    }
+}
+// log(wrapAdjective("||")("a dedicated programmer"));
+// log(wrapAdjective()("a hard worker"));
+// log(wrapAdjective()());
+// log(wrapAdjective('||')());
